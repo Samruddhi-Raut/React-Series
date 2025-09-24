71ce8f893d4bdb098be3784e806bad4e4055fb7b
Samruddhi-Raut <samruddhiraut1808@gmail.com>
2025-09-24 13:25:49 +0530

    Add 04bgChanger feature with background toggle functionality

--- 04bgChanger/basics/src/chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return(
-        <h3>Good morning</h3>
-    )
-}
-
-export default Chai;
--- 04bgChanger/reactbasics/src/Chai.js
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return (
-        <h1>Hola Amigo</h1>
-    )
-}
-
-export default Chai;
